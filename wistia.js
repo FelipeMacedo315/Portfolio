8238ce2082e37e0beb6786108c019184e297529e
FelipeMacedo315 <79103248+FelipeMacedo315@users.noreply.github.com>
2021-02-15 14:10:39 -0300

    Add files via upload

---
+++ wistia.js
@@ -0,0 +1,52 @@
+var input1=''
+var input2=''
+var input3=''
+var input4=''
+
+/*function caixa1() {
+    input1=document.getElementById('input1').value
+    
+   
+        
+    }
+ function caixa2() {
+ input2=document.getElementById('input2').value
+       
+            
+        }
+function caixa3() {
+    input3=document.getElementById('input3').value     
+            }
+ function caixa4() {
+ input4=document.getElementById('input4').value
+               
+                    
+  }*/
+  
+   function createaccount() {
+    input1=document.getElementById('input1').value
+    input2=document.getElementById('input2').value
+    input3=document.getElementById('input3').value 
+    input4=document.getElementById('input4').value
+
+    if (input1.length==0&&input2.length==0&&input3.length==0&&input4.length==0) {window.alert('prencha os campos')
+      
+    }
+    else{
+      
+    }
+    
+  }
+  
+
+
+
+   
+  
+    
+
+    
+    
+    
+
+
